876b52239d21953165d10ef485116bf9102e1999
Mathewski77 EDM-Portfolio_Mathew <mlacsina23-0408@cca.edu.ph>
2025-03-05 19:16:05 +0800

    Update container.php

--- container.php
+++ container.php
@@ -13,6 +13,15 @@
             <p>This part is inside a .container class.</p>
             <p>The .container class provides a responsive fixed width container.</p>
             </div>
+            
+            <div class="container">
+                <p class="h1">h1 Bootstrap heading</p>
+                <p class="h2">h2 Bootstrap heading</p>
+                <p class="h3">h3 Bootstrap heading</p>
+                <p class="h4">h4 Bootstrap heading</p>
+                <p class="h5">h5 Bootstrap heading</p>
+                <p class="h6">h6 Bootstrap heading</p>
 
+            </div>
         </body>
 </html>
